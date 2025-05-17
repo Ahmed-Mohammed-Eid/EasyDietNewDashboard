b73c40a28d7d261a340dd7f077ed4f0d8261e3be
Ahmed Mohammed <AhmedIsmail73990@azhar.edu.eg>
2025-05-17 15:28:07 +0300

    Update API URLs in next.config.js and remove outdated rules.md

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: 'https://api.your-couch.info/api/v1',
-        WEBSITE_URL: 'https://api.your-couch.info'
+        API_URL: 'https://test.easydietkw.com/api/v1',
+        WEBSITE_URL: 'https://test.easydietkw.com'
     },
     images: {
-        domains: ['kportals.net', 'easydiet.kportals.net', 'api.easydietkw.com', 'localhost', 'api.your-couch.info', 'your-couch.info']
+        domains: ['kportals.net', 'easydiet.kportals.net', 'api.easydietkw.com', 'localhost', 'test.easydietkw.com', 'easydietkw.com', 'www.easydietkw.com', 'cdn.easydietkw.com', 'cdn.kportals.net']
     }
 };
 
